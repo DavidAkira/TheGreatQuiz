d4d7670eb75c679dc999754e280af13fb23ea00f
David Kihlstrand <david.kihlstrand@outlook.com>
2016-03-08 21:18:50 +0100

    Uppdaterat Quizpage, lagt till Json och angular funktionalitet

--- TheGreatQuiz/Scripts/quiz.js
+++ TheGreatQuiz/Scripts/quiz.js
@@ -1,1 +1,21 @@
-﻿
+﻿(function() {
+
+    var app = angular.module("myQuiz",[]);
+
+    app.controller("QuizController", ["$scope", "$http", "$sce", function($scope, $http, $sce) {
+
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
+
+        $http.get("http://localhost:3434/JsonData/Quiz.json").then(function (quizData) {
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
+
+
+    }]);
+
+
+})();
