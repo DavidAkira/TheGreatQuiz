cbcab46c8ee7d5f4bdc37f994cb6bbdae2e86cc0
David Kihlstrand <david.kihlstrand@outlook.com>
2016-03-16 20:41:42 +0100

    Added ResultPage

--- TheGreatQuiz/Scripts/quiz.js
+++ TheGreatQuiz/Scripts/quiz.js
@@ -18,10 +18,11 @@
         });
 
         $http.get("/Home/LoadQuizData").success(function (data) {
-            console.log(data);
+            
             $scope.quizTitle = data.Name;             
             quizTimer = data.QuizTimer;
-            $scope.showAnswers = data.showAnswers;
+            $scope.showAnswers = data.ShowAnswers;
+            console.log(data);
         });
 
         $scope.selectAnswer = function(questionIndex, answerIndex) {
